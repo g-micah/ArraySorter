61a0dce883a4d44a015cfec712461c4bc596447c
Micah Garrison <micah@micahgarrison.com>
2021-02-25 11:28:43 -0500

    Add buttons for sorts and test merge sort

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import * as sortingAlgorithms from '../sortingAlgorithms/sortingAlgorithms.js';
 import './SortingVisualizer.css';
 
 export default class SortingVisualizer extends React.Component {
@@ -17,11 +18,40 @@ export default class SortingVisualizer extends React.Component {
     resetArray() {
         const array = [];
         for (let i = 0; i < 310; i++) {
-            array.push(randomIntFromInterval(5, 500));
+            array.push(randomIntFromInterval(5, 580));
         }
         this.setState({array});
     }
 
+    mergeSort(){
+        const sortedArray = sortingAlgorithms.mergeSort(this.state.array); 
+
+
+
+        const javaScriptSortedArray = this.state.array.slice().sort((a,b) => a - b);
+        console.log(arraysAreEqual(javaScriptSortedArray, sortedArray));
+    }
+
+    quickSort(){
+
+    }
+
+    heapSort(){
+
+    }
+
+    bubbleSort(){
+
+    }
+
+    insertionSort(){
+
+    }
+
+    testSortingAlgorithm(){
+
+    }
+    
     render() {
         const {array} = this.state;
 
@@ -34,8 +64,15 @@ export default class SortingVisualizer extends React.Component {
                         style={{height: `${value}px`}}>
                     </div>
                 ))}
+                <br />
+                <button onClick={() => this.resetArray()}>Generate New Array</button>
+                <button onClick={() => this.mergeSort()}>Merge Sort</button>
+                <button onClick={() => this.quickSort()}>Quick Sort</button>
+                <button onClick={() => this.heapSort()}>Heap Sort</button>
+                <button onClick={() => this.bubbleSort()}>Bubble Sort</button>
+                <button onClick={() => this.insertionSort()}>Insertion Sort</button>
 
-                
+
             </div>
         );
     }
@@ -43,4 +80,15 @@ export default class SortingVisualizer extends React.Component {
 
 function randomIntFromInterval(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function arraysAreEqual(arrayOne, arrayTwo) {
+    if (arrayOne.length !== arrayTwo.length) return false;
+    for (let i = 0; i < arrayOne.length; i++) {
+        if (arrayOne[i] !== arrayTwo[i]){
+            return false;
+        }
+        
+    }
+    return true;
 }
